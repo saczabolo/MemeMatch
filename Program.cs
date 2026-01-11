83f7f831494df0c66ae5c717412d28824abdd37c
mondu <monikadubiel@onet.pl>
2026-01-11 17:00:25 +0100

    front problematyczny vol.2

--- Program.cs
+++ Program.cs
@@ -9,11 +9,16 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+
+builder.Services.AddControllersWithViews();
+
+
 builder.Services.AddDbContext<AppDbContext>(option =>
-option.UseSqlite("Data Source=memematch.db"));
+    option.UseSqlite("Data Source=memematch.db"));
+
 
 builder.Services.AddScoped<GameService>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -24,9 +29,12 @@ builder.Services.AddSwaggerGen(c =>
         Scheme = "Bearer",
         BearerFormat = "JWT",
         In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+        Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n" +
+                      "Enter 'Bearer' [space] and then your token.\r\n\r\n" +
+                      "Example: \"Bearer 1safsfsdfdfd\"",
     });
 });
+
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -43,6 +51,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 var app = builder.Build();
+
 
 using (var scope = app.Services.CreateScope())
 {
@@ -71,10 +80,21 @@ using (var scope = app.Services.CreateScope())
     context.SaveChanges();
 }
 
+
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseStaticFiles();
+
 app.UseAuthentication();
 app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 app.MapControllers();
 
 app.Run();
